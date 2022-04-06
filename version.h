fb4c41ea2e030fc366a2cd0a8a846f7b091ac145
Carlo <carlo.lucchesi@stud.unifi.it>
2022-04-06 15:49:22 +0200

    Aggiunto codice versione programma.

---
+++ version.h
@@ -0,0 +1,12 @@
+//
+// Created by carlo on 06/04/22.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
